d333df9fa5defef4f5ba9008ce87b3e5b1b3655d
Colin Burr <colin.a.burr@gmail.com>
2019-08-13 10:20:02 -0700

    Add circle viz

--- scripts/canvasvis.js
+++ scripts/canvasvis.js
@@ -9,16 +9,16 @@ const visEl = document.querySelector('#visual');
 const dpr = window.devicePixelRatio;
 // get window dimensions & set canvas to fill window
 function Dimensions() {
-	this.width = (window.innerWidth)*dpr;
-	this.height = (window.innerHeight)*dpr;
+	this.width = (window.innerWidth - 100)*dpr;
+	this.height = (window.innerHeight - 100)*dpr;
 	this.centerX = this.width/2;
 	this.centerY = this.height/2;
-	
+
 	this.setFullscreen = function(el) {
 		el.width = this.width;
 		el.height = this.height;
 	}
-	
+
 	this.update = function() {
 		this.width = (window.innerWidth)*dpr;
 		this.height = (window.innerHeight)*dpr;
@@ -37,26 +37,31 @@ const ctx = visEl.getContext('2d');
 ctx.lineWidth = 0.0;
 ctx.lineCap = 'round';
 ctx.lineJoin = 'round';
+ctx.fillStyle = 'white';
+ctx.fillRect(0, 0, screenDim.width, screenDim.height);
 
 const binSize = 128;
 const elAmount = Math.floor(binSize/3); // Returned frequncies is a third
 
 // create a new vis
-const vis = new Vis(binSize, '/beast.mp3');
+const vis = new Vis(binSize, '233305303');
 
 // setup our draw loop: THIS IS WHERE THE MAGIC HAPPENS!!
 vis.draw( () => {
 
-	ctx.clearRect(0, 0, screenDim.width, screenDim.height);
+  ctx.clearRect(0, 0, screenDim.width, screenDim.height);
+  ctx.fillStyle = 'white';
+  ctx.fillRect(0, 0, screenDim.width, screenDim.height);
+
 	// loop over our frequencies and draw a shape for each one
 	vis.frequencies.forEach((f, i) => {
 		ctx.beginPath();
-		ctx.fillStyle = 'red';
-		ctx.arc(i*20, i*20, f, 0, 5);
+		ctx.fillStyle = `hsla(${i*(360 / elAmount)}, 50%, 50%, 1)`;
+		ctx.arc(screenDim.width/elAmount * i, screenDim.height/elAmount * i, f, 0, 2 * Math.PI);
 		ctx.fill();
 		ctx.closePath();
 	})
-	
+
 } )
 
 
@@ -72,5 +77,5 @@ controls.querySelector('[data-control="play"]').addEventListener('click', functi
     this.dataset.on = "false";
     vis.stop();
 	}
-   
+
 })
